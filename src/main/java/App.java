9465b6f767b792bd31697789afee886df7a28520
wcourtney31415 <wcourtney31415@gmail.com>
2020-11-18 00:31:16 -0500

    Passed in the username and password through request headers.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,42 +7,52 @@ import java.util.concurrent.TimeUnit;
 import com.google.gson.Gson;
 
 public class App {
-	
-	private static Date epochToDate (Long epoch) {
+
+	private static Date epochToDate(Long epoch) {
 		Date expiry = new Date(epoch * 1000);
 		return expiry;
 	}
-	
+
 	private static long deltaInSeconds(Date startTime, Date endTime) {
 		long diffInMillies = Math.abs(endTime.getTime() - startTime.getTime());
-	    long diff = TimeUnit.SECONDS.convert(diffInMillies, TimeUnit.MILLISECONDS);
+		long diff = TimeUnit.SECONDS.convert(diffInMillies, TimeUnit.MILLISECONDS);
 		return diff;
 	}
-	
+
 	private static String secondsToTimeString(long seconds) {
-        long hours = seconds / 60 / 60;
-        long minutes = (seconds / 60) % 60;
-        String ret =  hours + " hours and " + minutes + " minutes";
-        return ret;
+		long hours = seconds / 60 / 60;
+		long minutes = (seconds / 60) % 60;
+		String ret = hours + " hours and " + minutes + " minutes";
+		return ret;
 	}
-	
+
 	private static String actualUsername = "bob";
 	private static String actualPassword = "passwordy";
-	
+
 	public static void main(String[] args) {
 		port(80);
+
 		get("/", (req, res) -> "Welcome home.");
 
 		get("/something", (req, res) -> {
-			String body = req.body();
-			Gson gson = new Gson();
-			TimeSegment timeSegment = gson.fromJson(body, TimeSegment.class);
-			Date startTime = epochToDate(timeSegment.startTime);
-			Date endTime = epochToDate(timeSegment.endTime);
-			long diff = deltaInSeconds(startTime, endTime);
-			String strDiff = secondsToTimeString(diff);
-			System.out.println(strDiff);
-			return strDiff;
+			String username = req.headers("username");
+			String password = req.headers("password");
+
+			if (username.equals(actualUsername) && password.equals(actualPassword)) {
+				String body = req.body();
+				Gson gson = new Gson();
+				TimeSegment timeSegment = gson.fromJson(body, TimeSegment.class);
+				Date startTime = epochToDate(timeSegment.startTime);
+				Date endTime = epochToDate(timeSegment.endTime);
+				long diff = deltaInSeconds(startTime, endTime);
+				String strDiff = secondsToTimeString(diff);
+				System.out.println(strDiff);
+				return strDiff;
+			} else {
+				res.redirect("http://www.google.com");
+				return res;
+			}
+
 		});
 
 	}
