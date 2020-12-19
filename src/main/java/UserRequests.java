f6993344f27f4119df50865ad3d3f1e5e95341e2
wcourtney31415 <wcourtney31415@gmail.com>
2020-12-19 01:30:36 -0500

    Used a lambda to remove some of the boilerplate.

--- src/main/java/UserRequests.java
+++ src/main/java/UserRequests.java
@@ -2,6 +2,8 @@ import static spark.Spark.get;
 import static spark.Spark.post;
 
 import java.util.Collection;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 import org.bson.Document;
 import org.bson.conversions.Bson;
@@ -14,21 +16,28 @@ import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
 
 public class UserRequests {
+	static String dbName = "Time";
+	static String collectionName = "User";
+
+	static void doStuff(Consumer<MongoCollection<Document>> con) {
+		MongoClient mongoClient = new MongoClient();
+		MongoDatabase database = mongoClient.getDatabase(dbName);
+		MongoCollection<Document> collection = database.getCollection(collectionName);
+		con.accept(collection);
+		mongoClient.close();
+	}
 
 	public static void initialize() {
 
 		get("/user", (req, res) -> {
 			String idString = req.params("id");
-			String dbName = "Time";
-			String collectionName = "User";
-			MongoClient mongoClient = new MongoClient();
-			MongoDatabase database = mongoClient.getDatabase(dbName);
-			MongoCollection<Document> collection = database.getCollection(collectionName);
-			Bson filter = Filters.eq("lastName", "Platipus");
-			FindIterable<Document> result = collection.find(filter);
-			Document document = result.first();
-			Object firstName = document.get("firstName");
-			mongoClient.close();
+			Object firstName = null;
+			doStuff((collection) -> {
+				Bson filter = Filters.eq("lastName", "Platipus");
+				FindIterable<Document> result = collection.find(filter);
+				Document document = result.first();
+				firstName = document.get("firstName");
+			});
 			res.status(200);
 			return String.valueOf(firstName);
 		});
