28debb4dec475ddcfe9170419a40312e083712ef
wcourtney31415@gmail.com <wcourtney31415@gmail.com>
2021-04-12 05:09:51 -0400

    Began adding a UI to interpret the REST API portion of the projec. Also added a second toString method for producing elegant output for the listbox.

---
+++ src/main/java/UI/MainWindow.java
@@ -0,0 +1,122 @@
+package UI;
+
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+public class MainWindow {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainWindow window = new MainWindow();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MainWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 705, 460);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+
+		JLabel lblWelcome = new JLabel("Welcome John Smith");
+		lblWelcome.setBounds(10, 11, 170, 14);
+		frame.getContentPane().add(lblWelcome);
+
+		JComboBox cmbBox = new JComboBox();
+		cmbBox.setBounds(10, 36, 135, 22);
+		frame.getContentPane().add(cmbBox);
+
+		JList lstTimeSegments = new JList();
+		lstTimeSegments.setBounds(10, 69, 354, 341);
+		frame.getContentPane().add(lstTimeSegments);
+
+		JButton btnFetchTimeSegments = new JButton("Fetch Time Segments");
+		btnFetchTimeSegments.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String uri = "http://localhost/timeSegments";
+				List<resources.TimeSegment> timeSegments = getTimeSegments(uri);
+				List<String> timeSegmentStrings = new ArrayList<String>();
+				for (resources.TimeSegment timeSegment : timeSegments) {
+					timeSegmentStrings.add(timeSegment.toPrettyString());
+				}
+				updateList(lstTimeSegments, timeSegmentStrings);
+
+			}
+		});
+		btnFetchTimeSegments.setBounds(229, 36, 135, 23);
+		frame.getContentPane().add(btnFetchTimeSegments);
+	}
+
+	private static List<resources.TimeSegment> getTimeSegments(String url) {
+		HttpClient client = HttpClient.newHttpClient();
+
+		HttpRequest request = HttpRequest.newBuilder(URI.create(url)).header("accept", "application/json").build();
+
+		List<resources.TimeSegment> outputList = new ArrayList<resources.TimeSegment>();
+
+		HttpResponse<?> response = null;
+		try {
+			response = client.send(request, BodyHandlers.ofString());
+			Gson gson = new Gson();
+
+			List<resources.TimeSegment> inputList = new ArrayList<resources.TimeSegment>();
+
+			java.lang.reflect.Type listOfMyClassObject = new TypeToken<ArrayList<resources.TimeSegment>>() {
+			}.getType();
+
+			outputList = gson.fromJson((String) response.body(), listOfMyClassObject);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		} catch (InterruptedException e1) {
+			e1.printStackTrace();
+		}
+		return outputList;
+	}
+
+	private static void updateList(JList<String> jlist, List<String> arrList) {
+		DefaultListModel<String> model = new DefaultListModel<String>();
+		model.addAll(arrList);
+		jlist.setModel(model);
+	}
+
+}
--- src/main/java/resources/TimeSegment.java
+++ src/main/java/resources/TimeSegment.java
@@ -1,5 +1,10 @@
 package resources;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 import org.bson.types.ObjectId;
 
 import com.google.gson.Gson;
@@ -14,6 +19,16 @@ public class TimeSegment {
 		Gson gson = new Gson();
 		
 		return gson.toJson(this);
+	}
+	
+	public String toPrettyString() {
+		DateFormat format = new SimpleDateFormat("MM/dd/yyyy hh:mm aa");
+		format.setTimeZone(TimeZone.getTimeZone("Etc/UTC"));
+		Date startDateTime = new Date(startTime);
+        String formattedStartTime = format.format(startDateTime);
+        Date endDateTime = new Date(endTime);
+        String formattedEndTime = format.format(endDateTime);
+		return formattedStartTime + " - " + formattedEndTime;
 	}
 
 }
