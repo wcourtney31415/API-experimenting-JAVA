d38a7fabcbdbb5eede7d260ed43aea23b731519c
wcourtney31415 <wcourtney31415@gmail.com>
2021-04-13 23:47:41 -0400

    Trying to eliminate warnings, most of which were unspecified type parameters, while also cleaning up the code a bit.

--- src/main/java/UI/MainWindow.java
+++ src/main/java/UI/MainWindow.java
@@ -29,6 +29,7 @@ import org.slf4j.LoggerFactory;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+import resources.TimeSegment;
 import routing.BoxRouting;
 import routing.TimeSegmentRouting;
 import routing.UserRouting;
@@ -37,6 +38,7 @@ public class MainWindow {
 
 	private JFrame frame;
 
+	@SuppressWarnings("unused")
 	private static final Logger logger = LoggerFactory.getLogger(MainWindow.class);
 
 	/**
@@ -82,11 +84,11 @@ public class MainWindow {
 		lblWelcome.setBounds(10, 11, 170, 14);
 		frame.getContentPane().add(lblWelcome);
 
-		JComboBox cmbBox = new JComboBox();
+		JComboBox<String> cmbBox = new JComboBox<String>();
 		cmbBox.setBounds(10, 36, 135, 22);
 		frame.getContentPane().add(cmbBox);
 
-		JList lstTimeSegments = new JList();
+		JList<String> lstTimeSegments = new JList<String>();
 		lstTimeSegments.setBounds(10, 69, 354, 341);
 		frame.getContentPane().add(lstTimeSegments);
 
@@ -94,9 +96,9 @@ public class MainWindow {
 		btnFetchTimeSegments.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String uri = "http://localhost/timeSegment";
-				List<resources.TimeSegment> timeSegments = getTimeSegments(uri);
+				List<TimeSegment> timeSegments = getTimeSegments(uri);
 				List<String> timeSegmentStrings = new ArrayList<String>();
-				for (resources.TimeSegment timeSegment : timeSegments) {
+				for (TimeSegment timeSegment : timeSegments) {
 					timeSegmentStrings.add(timeSegment.toPrettyString());
 				}
 				updateList(lstTimeSegments, timeSegmentStrings);
@@ -106,31 +108,29 @@ public class MainWindow {
 		btnFetchTimeSegments.setBounds(229, 36, 135, 23);
 		frame.getContentPane().add(btnFetchTimeSegments);
 	}
-
-	private static List<resources.TimeSegment> getTimeSegments(String url) {
+	
+	private static List<TimeSegment> getTimeSegments(String url) {
+		
+		List<TimeSegment> timeSegments = new ArrayList<TimeSegment>();
+		
 		HttpClient client = HttpClient.newHttpClient();
-
 		HttpRequest request = HttpRequest.newBuilder(URI.create(url)).header("accept", "application/json").build();
-
-		List<resources.TimeSegment> outputList = new ArrayList<resources.TimeSegment>();
-
-		HttpResponse<?> response = null;
+		HttpResponse<String> response = null;
+		
 		try {
 			response = client.send(request, BodyHandlers.ofString());
-			Gson gson = new Gson();
-
-			List<resources.TimeSegment> inputList = new ArrayList<resources.TimeSegment>();
-
-			Type listOfMyClassObject = new TypeToken<ArrayList<resources.TimeSegment>>() {
+			
+			Type listOfMyClassObject = new TypeToken<ArrayList<TimeSegment>>() {
 			}.getType();
-
-			outputList = gson.fromJson((String) response.body(), listOfMyClassObject);
+			
+			Gson gson = new Gson();
+			timeSegments = gson.fromJson((String) response.body(), listOfMyClassObject);
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		} catch (InterruptedException e1) {
 			e1.printStackTrace();
 		}
-		return outputList;
+		return timeSegments;
 	}
 
 	private static void updateList(JList<String> jlist, List<String> arrList) {
